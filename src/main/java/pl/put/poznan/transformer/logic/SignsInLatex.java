acf9cb12960cc4b6cf99ab3216ffdf1eef8a1c26
Beata Wiśniewska <beatawis20@gmail.com>
2021-12-18 17:36:08 +0100

    Update SignsInLatex.java

--- src/main/java/pl/put/poznan/transformer/logic/SignsInLatex.java
+++ src/main/java/pl/put/poznan/transformer/logic/SignsInLatex.java
@@ -9,7 +9,16 @@ public class SignsInLatex {
 
     public String[] transform()
     {
-        //Transformer logic here
-        return new String[0];
+        String[] signs = {"\\$", "&", "#", "%", "\\{", "\\}"};
+
+        for (int i = 0; i < this.text.length; i++) {
+
+            this.text[i] = this.text[i].replaceAll("\\\\","\\\\backslash");
+
+            for (int j = 0; j < signs.length; j++) {
+                this.text[i] = this.text[i].replaceAll(signs[i], "\\\\" + signs[i]);
+            }
+        }
+        return this.text;
     }
 }
