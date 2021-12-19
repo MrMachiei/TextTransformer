ea9252c32a67c047a555e4edb36ce506a5d8bec7
Karol Zawiślak <karol.zawislak@student.put.poznan.pl>
2021-12-19 22:35:50 +0100

    fix inverse function change String list to string

--- src/main/java/pl/put/poznan/transformer/logic/Inverse.java
+++ src/main/java/pl/put/poznan/transformer/logic/Inverse.java
@@ -1,41 +1,31 @@
 package pl.put.poznan.transformer.logic;
 
 public class Inverse {
-    private final String[] text;
+    private String text;
 
-    public Inverse(String[] text){
+    public Inverse(String text){
         this.text = text;
     }
 
-    public String[] transform()
-    {
+    public String transform() {
 
-    String reversed;
-    for(int i = 0; i < this.text.length; i++)
-    {
-      Boolean[] upper = new Boolean[this.text[i].length()];
-      
-      for(int j = 0; j < this.text[i].length(); j++)
-      {
-        if(this.text[i].charAt(j) > 'A' && this.text[i].charAt(j) < 'Z'){
-        	upper[j] = true;
-        }
-        else{
-        	upper[j] = false;
-        }
-      }
+        String reversed;
 
-      reversed = "";
-      for(int j = 0; j < this.text[i].length(); j++)
-      {
-        if(!upper[j]){
-        	reversed += Character.toString(this.text[i].charAt(this.text[i].length()-j-1)).toLowerCase();
-        }
-        else{
-        	reversed += Character.toString(this.text[i].charAt(this.text[i].length()-j-1)).toUpperCase();
+        Boolean[] upper = new Boolean[this.text.length()];
 
+        for (int j = 0; j < this.text.length(); j++) {
+            upper[j] = this.text.charAt(j) > 'A' && this.text.charAt(j) < 'Z';
         }
-      }
+
+        reversed = "";
+        for (int j = 0; j < this.text.length(); j++) {
+            if (!upper[j]) {
+                reversed += Character.toString(this.text.charAt(this.text.length() - j - 1)).toLowerCase();
+            } else {
+                reversed += Character.toString(this.text.charAt(this.text.length() - j - 1)).toUpperCase();
+            }
+        }
+        this.text = reversed;
         return this.text;
     }
 }
