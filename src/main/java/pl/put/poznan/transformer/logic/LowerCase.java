23772493be904a4310d57930701d61766732f33d
Karol Zawiślak <karol.zawislak@student.put.poznan.pl>
2021-12-21 17:05:13 +0100

    add doc to LowerCase class

--- src/main/java/pl/put/poznan/transformer/logic/LowerCase.java
+++ src/main/java/pl/put/poznan/transformer/logic/LowerCase.java
@@ -1,11 +1,34 @@
 package pl.put.poznan.transformer.logic;
 
+import java.rmi.NoSuchObjectException;
+
+/**
+ * Klasa jest odpowiedzialna za funkjonalnosc zamiany wszystkich liter tekstu na male
+ * klasa posiada - text, przechowujacy transformowana informacje
+ *
+ * @author Karol Zawislak
+ * @version 2.2
+ */
+
+
 public class LowerCase {
     private final String text;
+
+    /**
+     * Konstruktor klasy
+     *
+     * @param text transformowany tekst
+     */
 
     public LowerCase(String text){
         this.text = text;
     }
+
+    /**
+     * metoda odpowiedzialna za transformacje obiektu
+     *
+     * @return tekst po zamianie lister na male
+     */
 
     public String transform()
     {
