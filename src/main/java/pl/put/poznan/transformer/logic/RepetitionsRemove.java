07d8aa35aec66c99b33220f782acebae82b183ad
Karol Zawiślak <karol.zawislak@student.put.poznan.pl>
2022-01-08 17:41:42 +0100

    prepare to decorator

--- src/main/java/pl/put/poznan/transformer/logic/RepetitionsRemove.java
+++ src/main/java/pl/put/poznan/transformer/logic/RepetitionsRemove.java
@@ -1,17 +1,18 @@
 package pl.put.poznan.transformer.logic;
 import java.util.StringTokenizer;
 
-public class RepetitionsRemove {
-    private final String text;
+public class RepetitionsRemove extends TextTransformer{
+    private final TextTransformer trans;
 
-    public RepetitionsRemove(String text){
-        this.text = text;
+    public RepetitionsRemove(TextTransformer trans){
+        this.trans = trans;
     }
 
+    @Override
     public String transform()
     {
         StringBuilder out = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(this.text);
+        StringTokenizer st = new StringTokenizer(this.trans.transform());
 
         String last = "";
         String current = "";
