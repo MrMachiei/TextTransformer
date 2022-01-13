90e24936df15c70ba2ca3d039ad93a3ddd3c079e
Beata Wiśniewska <beatawis20@gmail.com>
2022-01-13 21:11:58 +0100

    add documentation to SignsInLatex class

--- src/main/java/pl/put/poznan/transformer/logic/SignsInLatex.java
+++ src/main/java/pl/put/poznan/transformer/logic/SignsInLatex.java
@@ -1,11 +1,30 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * Klasa jest odpowiedzialna za zapisanie znaków w taki sposób, aby były one kompatybilne z latexem
+ *
+ * @author Beata Wiśniewska
+ * @version 3.4
+ */
+
 public class SignsInLatex extends TextTransformer{
     private final TextTransformer trans;
+
+    /**
+     * Konstruktor klasy
+     *
+     * @param trans - nadrzedny dekorator
+     */
 
     public SignsInLatex(TextTransformer trans){
         this.trans = trans;
     }
+
+    /**
+     * metoda odpowiedzialna za zmienienie znaków na wersję odpowiednią w latexie -> dodanie \ przed znaki specjalne
+     *
+     * @return tekst po zamianie znaków
+     */
 
     public String transform()
     {
