f8d96ee00b1413b03b19c5e83730ec9c59bb75ad
mrmachiei <maciej.jacek.w@gmail.com>
2022-01-24 10:25:20 +0100

    Testy

--- src/test/pl/put/poznan/transformer/logic/ExtendShortcutsTest.java
+++ src/test/pl/put/poznan/transformer/logic/ExtendShortcutsTest.java
@@ -1,5 +1,7 @@
 package pl.put.poznan.transformer.logic;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,12 +9,39 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ExtendShortcutsTest {
+    TextTransformer mock = null;
+    @BeforeEach
+    void  setUp(){
+        mock = mock(TextTransformer.class);
+    }
 
     @Test
-    void transform() {
-        TextTransformer mock = mock(TextTransformer.class);
-        when(mock.transform()).thenReturn("MGr Inż. dR powiedzial.");
-        ExtendShortcuts trans = new ExtendShortcuts(mock);
-        assertEquals("MAgister Inżynier dOktor powiedzial.", trans.transform());
+    void testTransform() {
+        when(mock.transform()).thenReturn("MAgiSter Inżynier dOktor powiedzial.");
+        CollapseToShortcuts trans = new CollapseToShortcuts(mock);
+        assertEquals("MGr Inż. dR powiedzial.", trans.transform());
+    }
+
+    @Test
+    void testTransform1() {
+        when(mock.transform()).thenReturn("MaGister chodzi noca po klubach");
+        CollapseToShortcuts trans = new CollapseToShortcuts(mock);
+        assertEquals("MgR chodzi noca po klubach", trans.transform());
+    }
+
+    @Test
+    void testTransformAdvanced() {
+        when(mock.transform()).thenReturn("MGr DR habilitowany lubi oblewac studentow");
+        ExtendShortcuts extender = new ExtendShortcuts(mock);
+        CollapseToShortcuts trans = new CollapseToShortcuts(extender);
+        assertEquals("MGr DR habilitowany lubi oblewac studentow", trans.transform());
+    }
+
+    @Test
+    void testTransformAdvanced2() {
+        when(mock.transform()).thenReturn("Magister profesor habilitowany lubi oblewac studentow");
+        ExtendShortcuts extender = new ExtendShortcuts(mock);
+        CollapseToShortcuts trans = new CollapseToShortcuts(extender);
+        assertEquals("Mgr prof. habilitowany lubi oblewac studentow", trans.transform());
     }
 }
