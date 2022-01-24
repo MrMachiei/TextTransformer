e58391e49dec8472a12b58292558cb5a84642bf6
mrmachiei <maciej.jacek.w@gmail.com>
2022-01-24 10:31:40 +0100

    Extend tests

--- src/test/pl/put/poznan/transformer/logic/ExtendShortcutsTest.java
+++ src/test/pl/put/poznan/transformer/logic/ExtendShortcutsTest.java
@@ -17,31 +17,31 @@ class ExtendShortcutsTest {
 
     @Test
     void testTransform() {
-        when(mock.transform()).thenReturn("MAgiSter Inżynier dOktor powiedzial.");
-        CollapseToShortcuts trans = new CollapseToShortcuts(mock);
-        assertEquals("MGr Inż. dR powiedzial.", trans.transform());
+        when(mock.transform()).thenReturn("MGr Inż. dR powiedzial.");
+        ExtendShortcuts trans = new ExtendShortcuts(mock);
+        assertEquals("MAgister Inżynier dOktor powiedzial.", trans.transform());
     }
 
     @Test
     void testTransform1() {
-        when(mock.transform()).thenReturn("MaGister chodzi noca po klubach");
-        CollapseToShortcuts trans = new CollapseToShortcuts(mock);
-        assertEquals("MgR chodzi noca po klubach", trans.transform());
+        when(mock.transform()).thenReturn("MgR chodzi noca po klubach");
+        ExtendShortcuts trans = new ExtendShortcuts(mock);
+        assertEquals("MaGister chodzi noca po klubach", trans.transform());
     }
 
     @Test
     void testTransformAdvanced() {
         when(mock.transform()).thenReturn("MGr DR habilitowany lubi oblewac studentow");
-        ExtendShortcuts extender = new ExtendShortcuts(mock);
-        CollapseToShortcuts trans = new CollapseToShortcuts(extender);
-        assertEquals("MGr DR habilitowany lubi oblewac studentow", trans.transform());
+        CollapseToShortcuts collapser = new CollapseToShortcuts(mock);
+        ExtendShortcuts trans = new ExtendShortcuts(collapser);
+        assertEquals("MAgister DOktor habilitowany lubi oblewac studentow", trans.transform());
     }
 
     @Test
     void testTransformAdvanced2() {
         when(mock.transform()).thenReturn("Magister profesor habilitowany lubi oblewac studentow");
-        ExtendShortcuts extender = new ExtendShortcuts(mock);
-        CollapseToShortcuts trans = new CollapseToShortcuts(extender);
-        assertEquals("Mgr prof. habilitowany lubi oblewac studentow", trans.transform());
+        CollapseToShortcuts collapser = new CollapseToShortcuts(mock);
+        ExtendShortcuts trans = new ExtendShortcuts(collapser);
+        assertEquals("Magister profesor habilitowany lubi oblewac studentow", trans.transform());
     }
 }
